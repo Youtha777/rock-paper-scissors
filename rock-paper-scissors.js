8e14c5507ea330d992ae6c98a1e08fde8f306dfd
Youtah <61557913+Youtha777@users.noreply.github.com>
2026-04-10 10:49:28 -0700

    Completed Step 2: Write the logic to get the computer choice and Added Pseudocode

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,59 @@
+//Each player choose between rock, paper or scissors
+//Rock beats scissors, scissors beat paper, and paper beats rock
+//IF rock
+  //IF rock
+    //result is a tie
+  //ELSE IF it is a paper
+    //result is a win
+    //check who selected paper and declare as winner
+  //ESLE IF it is a scissors
+    //result is lose
+    //check who selected rock and declare as winner
+//ELSE IF paper
+  //IF paper
+    //result is a tie
+  //ELSE IF rock
+    //result is lose
+    //check who selected paper and declare as winner
+  //ELSE IF scissors
+    //result is a win
+    //check who selected scissors and declare as winner
+//ELSE IF scissors
+  //IF scissors
+    //result is a tie
+  //ELSE IF rock
+    //result is a win
+    //check who selected rock and declare as winner
+  //ELSE IF paper
+    //result is lose
+    //check who selected paper and declare as winner
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log("Hello World!")
+
+//Invoke function to get computer's choice
+getComputerChoice()
+
+//Declare a function to generate the computer's choice
+function getComputerChoice()
+{
+  let choice = ["rock", "paper", "scissors"]
+
+  let random = Math.floor((Math.random() * 3))
+
+  return console.log(choice[random])
+}
+
